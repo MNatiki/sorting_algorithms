24f5ac1049638b7458b5bc4e15ccc5b36108c255
MNatiki <natiart07@gmail.com>
2023-07-25 05:21:14 -0700

    sorting

---
+++ 0-bubble_sort.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+
+
+/**
+ * bubble_sort - sorting an array of integers
+ *
+ * @array: The array to be printed
+ * @size: Number of elements in @array
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+    size_t i, j, temp;
+    for (i = 0; i < (size - 1); i++)
+    {
+        for (j = 0; j < (size - 1); j++)
+        {
+            if (array[j] > array[j + 1])
+            {
+                temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+                print_array(array, size);
+            }
+        }
+    }
+}
